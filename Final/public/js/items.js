dcbc42e7b0963ecff68ffb1adb866bbac1c5b011
chenwenjie <chrischen0405@gmail.com>
2018-06-24 03:48:58 +0800

    items界面提交

--- Final/public/js/items.js
+++ Final/public/js/items.js
@@ -1,1 +1,71 @@
 $("#menu").sideNav();
+var item1=new Vue({
+    el: "#item1",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/clarity_lg.png"},
+            {"name": "1", "url": "img/items/tango_lg.png"},
+            {"name": "1", "url": "img/items/flask_lg.png"}
+        ]
+    }
+})
+var item2=new Vue({
+    el: "#item2",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/branches_lg.png"},
+            {"name": "1", "url": "img/items/gauntlets_lg.png"},
+            {"name": "1", "url": "img/items/slippers_lg.png"}
+        ]
+    }
+})
+var item3=new Vue({
+    el: "#item3",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/ring_of_protection_lg.png"},
+            {"name": "1", "url": "img/items/stout_shield_lg.png"},
+            {"name": "1", "url": "img/items/quelling_blade_lg.png"}
+        ]
+    }
+})
+var item4=new Vue({
+    el: "#item4",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/magic_stick_lg.png"},
+            {"name": "1", "url": "img/items/sobi_mask_lg.png"},
+            {"name": "1", "url": "img/items/ring_of_regen_lg.png"}
+        ]
+    }
+})
+var item5=new Vue({
+    el: "#item5",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/wraith_band_lg.png"},
+            {"name": "1", "url": "img/items/null_talisman_lg.png"},
+            {"name": "1", "url": "img/items/magic_wand_lg.png"}
+        ]
+    }
+})
+var item6=new Vue({
+    el: "#item6",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/ring_of_basilius_lg.png"},
+            {"name": "1", "url": "img/items/headdress_lg.png"},
+            {"name": "1", "url": "img/items/recipe_urn_of_shadows_lg.png"}
+        ]
+    }
+})
+var item7=new Vue({
+    el: "#item7",
+    data: {
+        items: [
+            {"name": "1", "url": "img/items/veil_of_discord_lg.png"},
+            {"name": "1", "url": "img/items/force_staff_lg.png"},
+            {"name": "1", "url": "img/items/recipe_necronomicon_lg.png"}
+        ]
+    }
+})
