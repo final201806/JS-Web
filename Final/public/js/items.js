7f225cb475e15fd9ae2a9a5ba52336b883b1501d
chenwenjie <chrischen0405@gmail.com>
2018-06-24 19:28:48 +0800

    items界面修改

--- Final/public/js/items.js
+++ Final/public/js/items.js
@@ -1,71 +1,55 @@
 $("#menu").sideNav();
-var item1=new Vue({
-    el: "#item1",
+var div1=new Vue({
+    el: "#div1",
     data: {
-        items: [
-            {"name": "1", "url": "img/items/clarity_lg.png"},
-            {"name": "1", "url": "img/items/tango_lg.png"},
-            {"name": "1", "url": "img/items/flask_lg.png"}
+        divs: [
+            {"name": "消耗","url": "img/item/1.png","item": [
+                    {"name": "1", "url": "img/items/clarity_lg.png"},
+                    {"name": "1", "url": "img/items/tango_lg.png"},
+                    {"name": "1", "url": "img/items/flask_lg.png"}
+                ]},
+            {"name": "属性","url": "img/item/2.png","item": [
+                    {"name": "1", "url": "img/items/branches_lg.png"},
+                    {"name": "1", "url": "img/items/gauntlets_lg.png"},
+                    {"name": "1", "url": "img/items/slippers_lg.png"}
+                ]},
+            {"name": "初级","url": "img/item/2.png","item": [
+                    {"name": "1", "url": "img/items/ring_of_protection_lg.png"},
+                    {"name": "1", "url": "img/items/stout_shield_lg.png"},
+                    {"name": "1", "url": "img/items/quelling_blade_lg.png"}
+                ]},
+            {"name": "普通","url": "img/item/3.png","item": [
+                    {"name": "1", "url": "img/items/magic_stick_lg.png"},
+                    {"name": "1", "url": "img/items/sobi_mask_lg.png"},
+                    {"name": "1", "url": "img/items/ring_of_regen_lg.png"}
+                ]}
         ]
     }
 })
-var item2=new Vue({
-    el: "#item2",
+var div2=new Vue({
+    el: "#div2",
     data: {
-        items: [
-            {"name": "1", "url": "img/items/branches_lg.png"},
-            {"name": "1", "url": "img/items/gauntlets_lg.png"},
-            {"name": "1", "url": "img/items/slippers_lg.png"}
-        ]
-    }
-})
-var item3=new Vue({
-    el: "#item3",
-    data: {
-        items: [
-            {"name": "1", "url": "img/items/ring_of_protection_lg.png"},
-            {"name": "1", "url": "img/items/stout_shield_lg.png"},
-            {"name": "1", "url": "img/items/quelling_blade_lg.png"}
-        ]
-    }
-})
-var item4=new Vue({
-    el: "#item4",
-    data: {
-        items: [
-            {"name": "1", "url": "img/items/magic_stick_lg.png"},
-            {"name": "1", "url": "img/items/sobi_mask_lg.png"},
-            {"name": "1", "url": "img/items/ring_of_regen_lg.png"}
-        ]
-    }
-})
-var item5=new Vue({
-    el: "#item5",
-    data: {
-        items: [
-            {"name": "1", "url": "img/items/wraith_band_lg.png"},
-            {"name": "1", "url": "img/items/null_talisman_lg.png"},
-            {"name": "1", "url": "img/items/magic_wand_lg.png"}
-        ]
-    }
-})
-var item6=new Vue({
-    el: "#item6",
-    data: {
-        items: [
-            {"name": "1", "url": "img/items/ring_of_basilius_lg.png"},
-            {"name": "1", "url": "img/items/headdress_lg.png"},
-            {"name": "1", "url": "img/items/recipe_urn_of_shadows_lg.png"}
-        ]
-    }
-})
-var item7=new Vue({
-    el: "#item7",
-    data: {
-        items: [
-            {"name": "1", "url": "img/items/veil_of_discord_lg.png"},
-            {"name": "1", "url": "img/items/force_staff_lg.png"},
-            {"name": "1", "url": "img/items/recipe_necronomicon_lg.png"}
+        divs: [
+            {"name": "辅助","url": "img/item/4.png","item": [
+                    {"name": "1", "url": "img/items/wraith_band_lg.png"},
+                    {"name": "1", "url": "img/items/null_talisman_lg.png"},
+                    {"name": "1", "url": "img/items/magic_wand_lg.png"}
+                ]},
+            {"name": "武器","url": "img/item/5.png","item": [
+                    {"name": "1", "url": "img/items/ring_of_basilius_lg.png"},
+                    {"name": "1", "url": "img/items/headdress_lg.png"},
+                    {"name": "1", "url": "img/items/recipe_urn_of_shadows_lg.png"}
+                ]},
+            {"name": "法器","url": "img/item/5.png","item": [
+                    {"name": "1", "url": "img/items/veil_of_discord_lg.png"},
+                    {"name": "1", "url": "img/items/force_staff_lg.png"},
+                    {"name": "1", "url": "img/items/recipe_necronomicon_lg.png"}
+                ]},
+            {"name": "圣物","url": "img/item/6.png","item": [
+                    {"name": "1", "url": "img/items/magic_stick_lg.png"},
+                    {"name": "1", "url": "img/items/sobi_mask_lg.png"},
+                    {"name": "1", "url": "img/items/ring_of_regen_lg.png"}
+                ]}
         ]
     }
 })
